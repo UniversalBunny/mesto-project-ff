16157368650c8e4ce557b9cb6eabde27a715fa45
Alexandr <alexander.lemeni-makedon@yandex.kz>
2024-09-22 11:43:17 +0600

    first try

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard (cardData , deleteCard){
+        const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+
+        cardElement.querySelector('.card__image').src = cardData.link;
+        cardElement.querySelector('.card__title').textContent = cardData.name;
+
+        deleteButton.addEventListener('click', deleteCard); 
+    
+        return cardElement  
+}
+
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+}
+
+function addCards() {
+    initialCards.forEach(function(cardData){
+        cardContainer.append(createCard (cardData , deleteCard))
+    })
+}
+
+addCards()
