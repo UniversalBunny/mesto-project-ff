a1a75e8c72f63cbd53675a761f8aec49aa39c058
Александр Лемени-Македон <142801015+UniversalBunny@users.noreply.github.com>
2024-11-06 11:36:52 +0600

    PR7 reworked

--- src/components/api.js
+++ src/components/api.js
@@ -1,160 +1,122 @@
 const config = {
-    baseUrl: 'https://nomoreparties.co/v1/wff-cohort-25',
-    headers: {
-      authorization: 'b492c2c7-d2dc-4f84-ab81-75d472967bfc',
-      'Content-Type': 'application/json'
-    }
-  }
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-25",
+  headers: {
+    authorization: "b492c2c7-d2dc-4f84-ab81-75d472967bfc",
+    "Content-Type": "application/json",
+  },
+};
 
 // Для Promise.all([]) , в index.js
 
 const getCardsData = () => {
-    return fetch(`${config.baseUrl}/cards`, {
-        headers: config.headers
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-          return Promise.reject(`Что-то пошло не так: ${res.status}`);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-}
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
-const getId = () => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        headers: config.headers
-      })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      });  
-}
+const getProfileData = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
 // Редактирование профиля
 
 const updateUserInfo = (name, about) => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        method: 'PATCH',
-        headers: config.headers,
-        body: JSON.stringify({name ,about})
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({ name, about }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
 // добавление новой карточки
 
-const addCardRequest = (name, link) => {
-    return fetch(`${config.baseUrl}/cards`, {
-        method: 'POST',
-        headers: config.headers,
-        body: JSON.stringify({name , link})
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+const addCardRequest = (card) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: card.name,
+      link: card.link,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
-// Новый аватар 
+// Новый аватар
 
 const newAvatar = (avatarUrl) => {
-    return fetch(`${config.baseUrl}/users/me/avatar`, {
-        method: 'PATCH',
-        headers: config.headers,
-        body: JSON.stringify({avatar: avatarUrl})
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({ avatar: avatarUrl }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
 // Для card.js
 // Удаление карточки
 
 const deleteCardRequest = (cardId) => {
-    return fetch(`${config.baseUrl}/cards/${cardId}`, {
-        method: 'DELETE',
-        headers: config.headers
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
 //  Постановка и снятие лайка
 
 const addLike = (cardId, button, counter) => {
-    return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-        method: 'PUT',
-        headers: config.headers
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .then((res) => {
-        counter.textContent = res.likes.length;
-        button.classList.toggle('card__like-button_is-active');
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+    method: "PUT",
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
 const removeLike = (cardId, button, counter) => {
-    return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-        method: 'DELETE',
-        headers: config.headers
-    })
-    .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ${res.status}`);
-      })
-      .then((res) => {
-        counter.textContent = res.likes.length;
-        button.classList.toggle('card__like-button_is-active');
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-}
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  });
+};
 
-
-export { getCardsData, getId , updateUserInfo, addCardRequest, newAvatar, deleteCardRequest, addLike, removeLike};
+export { getCardsData, getProfileData , updateUserInfo, addCardRequest, newAvatar, deleteCardRequest, addLike, removeLike};
